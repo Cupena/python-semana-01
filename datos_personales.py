158c7f222b3f425fd8b23c18cbcddec4843faba2
cupena <cupena@uamv.edu.ni>
2026-08-18 08:40:36 -0600

    fin de solicitar datos personales

---
+++ datos_personales.py
@@ -0,0 +1,9 @@
+#Se le solicitara al visitante sus datos personales, tales como nombres, apellidos, carrera, edad, color favorito.
+
+nombreVisitante = input("Ingrese sus nombres: ")
+apellidosVisitante = input("Ingrese sus apellidos: ")
+edadVisitante = int(input("Ingrese su edad: "))
+carreraVisitante = input("Ingrese la carrera que esta cursando: ")
+colorFavoritoVisitante = input("Ingrese su color favorito: ")
+
+print("Hola", nombreVisitante, apellidosVisitante, "de edad: ", edadVisitante, "estudiante de la carrera: ", carreraVisitante, "con color favorito:", colorFavoritoVisitante)
