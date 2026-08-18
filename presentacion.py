f0aaae162d7071686d4c77b529a6adb20009be1c
cupena <cupena@uamv.edu.ni>
2026-08-18 08:31:34 -0600

    Finalice mi presentacion

--- presentacion.py
+++ presentacion.py
@@ -6,4 +6,4 @@ edad = 18
 carrera = "Ingenieria en Sistemas de Informacion"
 nacionalidad = "nicaragüense"
 
-print("Hola, mi nombre es", nombres, apellidos, "tengo la edad de:", edad)
+print("Hola, mi nombre es", nombres, apellidos, "tengo la edad de:", edad, "estudio la carrera de:", carrera, "ademas, mi nacionalidad es: ", nacionalidad)
