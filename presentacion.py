24b16fe7ab20dc9e706c647dc1242a5884c17ad2
cupena <cupena@uamv.edu.ni>
2026-08-18 08:28:54 -0600

    Agregue informacion personal

---
+++ presentacion.py
@@ -0,0 +1,9 @@
+#Presentacion del usuario creador del repositorio.
+
+nombres = "Carlos Uriel"
+apellidos = "Peña Méndez"
+edad = 18
+carrera = "Ingenieria en Sistemas de Informacion"
+nacionalidad = "nicaragüense"
+
+print("Hola, mi nombre es", nombres, apellidos, "tengo la edad de:", edad)
